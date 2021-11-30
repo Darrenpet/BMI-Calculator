91bad985f38fe6bd68566f5e5fc952f44580091e
Darrenpet <91587619+Darrenpet@users.noreply.github.com>
2021-11-30 13:40:39 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ function bmi() {
   let height = parseFloat(document.getElementById("height").value) / 100; // Convert height to meters
   let bmi = weight / (height * height); // According to the formula
   document.getElementById("answer").value =
-    String(bmi.toPrecision(3)) + " kg/m^2"; // writing to the output
+    String(bmi.toPrecision(3)) + "kg/m^2"; // writing to the output
 
   if (bmi < 18.5) {
     document.getElementById("category").value = "Underweight";
-  } else if (bmi >= 18.5 && b <= 25) {
+  } else if (bmi >= 18.5 && bmi <= 25) {
     document.getElementById("category").value = "Healthy weight";
-  } else if (bmi > 25 && b <= 30) {
+  } else if (bmi > 25 && bmi <= 30) {
     document.getElementById("category").value = "Overweight";
-  } else if (bmi > 30 && b <= 35) {
+  } else if (bmi > 30 && bmi <= 35) {
     document.getElementById("category").value = "Obese Class 1";
-  } else if (bmi > 35 && b <= 40) {
+  } else if (bmi > 35 && bmi <= 40) {
     document.getElementById("category").value = "Obese Class 2";
-  } else {
+  } else if (bmi > 40) {
     document.getElementById("category").value = "Obese Class 3";
   }
 }
